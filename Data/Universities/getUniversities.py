36d58a867c57a4db0b1f44c403a54eece44f4896
Wasim Boughattas <wassooma2013@yahoo.com>
2022-03-18 16:00:32 -0400

    commentate the getUniversities function and remove duplicate file

--- Data/Universities/getUniversities.py
+++ Data/Universities/getUniversities.py
@@ -1,34 +1,46 @@
 from SPARQLWrapper import SPARQLWrapper, CSV, JSON, XML, N3, RDFXML, TURTLE
 
-if __name__ == '__main__':
-    # Get all canadian universities' information
+
+def getUniversities():
+    """
+    connects to dbpedia's remote sparql endpoint and executes the following query.
+    The query outputs a list of all canadian universities and some information including:
+        dbpedia link
+        type
+        label
+        wikidata link
+    :return: rdf data in turtle format
+    """
     query = """
-            @prefix vivo: <http://vivoweb.org/ontology/core#> .
-            CONSTRUCT{
-            ?dbp_uri rdf:type vivo:University ;
-            rdfs:label ?name ;
-            owl:sameAs ?wd_uri .
-            }
-            WHERE {
-                {?dbp_uri rdf:type dbo:EducationalInstitution} 
-                    UNION
-                {?dbp_uri rdf:type schema:CollegeOrUniversity} .
+                @prefix vivo: <http://vivoweb.org/ontology/core#> .
+                CONSTRUCT{
+                ?dbp_uri rdf:type vivo:University ;
+                rdfs:label ?name ;
+                owl:sameAs ?wd_uri .
+                }
+                WHERE {
+                    {?dbp_uri rdf:type dbo:EducationalInstitution} 
+                        UNION
+                    {?dbp_uri rdf:type schema:CollegeOrUniversity} .
 
-                dbr:List_of_universities_in_Canada dbo:wikiPageWikiLink ?dbp_uri .
-                ?dbp_uri rdfs:label ?name .
-                ?dbp_uri owl:sameAs ?wd_uri .
+                    dbr:List_of_universities_in_Canada dbo:wikiPageWikiLink ?dbp_uri .
+                    ?dbp_uri rdfs:label ?name .
+                    ?dbp_uri owl:sameAs ?wd_uri .
 
-                FILTER(regex(str(?wd_uri), "www.wikidata.org"))
-                FILTER(lang(?name) = 'en')
-            } ORDER BY ?name
-            """
+                    FILTER(regex(str(?wd_uri), "www.wikidata.org"))
+                    FILTER(lang(?name) = 'en')
+                } ORDER BY ?name
+                """
 
     sparql = SPARQLWrapper('http://dbpedia.org/sparql')
     sparql.setQuery(query)
 
     sparql.setReturnFormat(N3)
     results = sparql.query().convert()
-    print(results)
 
+    return results
+
+
+if __name__ == '__main__':
     with open('Universities.ttl', 'wb') as f:
-        f.write(results)
+        f.write(getUniversities())
