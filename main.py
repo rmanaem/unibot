01107c15a6f3edba74f2f59a163dd54417709ffc
Wei Lien <70922579+weilien717@users.noreply.github.com>
2022-03-18 19:19:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,72 @@
 import requests
-from prettytable import PrettyTable
 
-#SPARQL printing the subject, predicate and object
-#query_var = 'SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object} LIMIT 25'
+def main():
+    # example query that returns all universities in the database
+    exampleQuery = """
+    PREFIX dbr: <http://dbpedia.org/resource/>
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX ns2: <http://vivoweb.org/ontology/core#>
+    SELECT * WHERE { 
+      ?university rdf:type ns2:University;
+    } LIMIT 25
+    """
+    executeQuey(exampleQuery)
 
-query_var = """
-PREFIX dbr: <http://dbpedia.org/resource/>
-PREFIX dbo: <http://dbpedia.org/ontology/>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX ns2: <http://vivoweb.org/ontology/core#>
+    #-----------------------------------competency q1-------------------------------------------------
+    # What is course [COURSE NAME][COURSE NUMBER] about?
+    courseName = None
+    courseNumber = None
+    qres1 = """
+        SELECT ?courseDesc
+        WHERE{{
+    		?x vivo:hasSubjectArea '{courseName}'
+    		?x vivo:Catalog '{courseNumber}'
+    		?x vivo:shortDescription ?courseDesc
+        }}
+    """
+    #executeQuey(qres1)
 
-SELECT * WHERE { 
-  ?university rdf:type ns2:University;
-} LIMIT 25
+    #-----------------------------------competency q2-------------------------------------------------
+    # Which topics is [STUDENT FIRSTNAME] [STUDENT LASTNAME]competent in?
+    givenName = None
+    familyName = None
+    qres2 = """
+    	SELECT ?competency
+    	WHERE{{
+    		?x foaf:givenName '{givenName}'
+    		?x foaf:familyName '{familyName}'
+    		?x docu:expertise ?y
+    		?y vivo:contains ?competency
+    	}}
+    """
+    # executeQuey(qres2)
 
-"""
+    #-----------------------------------competency q3-------------------------------------------------
+    # Which courses at [UNIVERSITY] teaches [TOPIC]
+    university = None
+    topic = None
+    qres3 = f"""
+    	SELECT ?course
+    	WHERE {{
+    		?x rdfs:label '{university}'
+    		?x vivo:offers ?course
+    		?course vivo:Title '{topic}'
+    	}}
+    """
+    #executeQuey(qres3)
 
-response = requests.post('http://localhost:3030/Project1/sparql',
-data={'query': query_var})
 
-res = response.json()
-#Prints the response of the SPARQL query
-print(res)
+def executeQuey(query):
+    #send post request to fuseki server to access dataset : Project1
+    response = requests.post('http://localhost:3030/Project1/sparql',
+                             data={'query': query})
+    res = response.json()
+    # Prints the response of the SPARQL query
+    print(res)
 
-#Lets try printing the values from the json output
-print("\nPrinting the values of subject predicate and object")
-'''
-print("---------------------------------------------------------------")
-for row in res['results']['bindings']:
-    print("Subject: ",row['subject']['value'])
-    print("Predicate: ",row['predicate']['value'])
-    print("Object: ",row['object']['value'])
-    print("")
-'''
-#t = PrettyTable(['Subject', 'Predicate','Object'])
-##t.align['Subject'] = "l"
-#t.align['Predicate'] = "l"
-#t.align['Object'] = "l"
-#for row in res['results']['bindings']:
-#    t.add_row([row['subject']['value'],row['predicate']['value'],row['object']['value']])
-#print(t)
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
