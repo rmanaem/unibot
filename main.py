1715ca478abf7ff0ad9243788c895289a1cdca0e
Wasim Boughattas <wassooma2013@yahoo.com>
2022-03-19 19:13:01 -0400

    main NOW executes every getRDF.py in the project, AND once the ttl files are generated, it merges them into a graph and exports the result in a ttl file in the parent directory called 'database.ttl'

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-# This script will execute each get_RDF.py file and export data directly to the server (instead of downloading
-# turtle files)
+import os
+import subprocess
+from fnmatch import fnmatch
+
+from rdflib import Graph
+
+from __init__ import ROOT_DIR
+
+if __name__ == '__main__':
+    TTL_pattern = "*.ttl"
+    GET_RDF_pattern = "getRDF.py"
+
+    g = Graph()
+    for path, subdir, files in os.walk(os.path.join(ROOT_DIR, 'Data')):
+        for name in files:
+            # runs every getRDF.py in the project
+            # RUN ONLY ONCE or DONT RUN if database.ttl already exists in parent directory
+            if fnmatch(name, GET_RDF_pattern):
+                pathname = os.path.join(path, name)
+                print('Running', pathname)
+                subprocess.run("python3 " + pathname, shell=True)
+
+            # merges the generated ttl files into one graph (database)
+            if fnmatch(name, TTL_pattern):
+                pathname = os.path.join(path, name)
+                g.parse(pathname)
+
+    database_pathname = os.path.join(ROOT_DIR, 'database.ttl')
+    g.serialize(database_pathname, format='turtle')
