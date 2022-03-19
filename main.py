129c0d092064668d14241bbd8d3329b3df2adaa0
n.richard <richard.n@latencetech.com>
2022-03-19 15:09:07 -0400

    Added remaining competency questions and regex checks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 def main():
     # example query that returns all universities in the database
@@ -13,50 +14,187 @@ def main():
     } LIMIT 25
     """
     executeQuey(exampleQuery)
-
+    print("""
+    Enter one of the questions below to test the knowledge base:
+    1. What is course [COURSE NAME][COURSE NUMBER] about?
+    2. Which topics is [STUDENT FIRSTNAME] [STUDENT LASTNAME] competent in?
+    3. Which courses at [UNIVERSITY] teaches [TOPIC].
+    4. What are all the courses for [COURSE NAME].
+    5. How many students are registered for [COURSE NAME][COURSE NUMBER]
+    6. Is [COURSE NAME][COURSE NUMBER] offered by [UNIVERSITY]?
+    7. Is [STUDENT FIRSTNAME][STUDENT LASTNAME] enrolled at [UNIVERSITY]?
+    8. Which universities offer [COURSE NAME][COURSE NUMBER].
+    9. What courses has [STUDENT FIRSTNAME][STUDENT LASTNAME] completed
+    10. What courses has [STUDENT FIRSTNAME][STUDENT LASTNAME] failed
+    """)
+    user_input = input()
     #-----------------------------------competency q1-------------------------------------------------
     # What is course [COURSE NAME][COURSE NUMBER] about?
-    courseName = None
-    courseNumber = None
-    qres1 = """
-        SELECT ?courseDesc
-        WHERE{{
-    		?x vivo:hasSubjectArea '{courseName}'
-    		?x vivo:Catalog '{courseNumber}'
-    		?x vivo:shortDescription ?courseDesc
-        }}
-    """
-    #executeQuey(qres1)
+    if re.search("^What is course.", user_input):
+        trimmed = re.sub("[.,?!]", "", user_input)
+        courseName = re.split("\s", trimmed)[3]
+        courseNumber = re.split("\s", trimmed)[4]
+        qres1 = f"""
+            SELECT ?courseDesc
+            WHERE{{
+    		    ?x vivo:hasSubjectArea '{courseName}'
+    		    ?x vivo:Catalog '{courseNumber}'
+    		    ?x vivo:shortDescription ?courseDesc
+            }}
+        """
+        #executeQuey(qres1)
 
     #-----------------------------------competency q2-------------------------------------------------
     # Which topics is [STUDENT FIRSTNAME] [STUDENT LASTNAME]competent in?
-    givenName = None
-    familyName = None
-    qres2 = """
-    	SELECT ?competency
-    	WHERE{{
-    		?x foaf:givenName '{givenName}'
-    		?x foaf:familyName '{familyName}'
-    		?x docu:expertise ?y
-    		?y vivo:contains ?competency
-    	}}
-    """
-    # executeQuey(qres2)
+    elif re.search("^Which topics is.", user_input):
+        trimmed = re.sub("[.,?!]", "", user_input)
+        givenName = re.split("\s", trimmed)[3]
+        familyName = re.split("\s", trimmed)[4]
+        qres2 = f"""
+    	    SELECT ?competency
+    	    WHERE{{
+    		    ?x foaf:givenName '{givenName}'
+    		    ?x foaf:familyName '{familyName}'
+    		    ?x docu:expertise ?y
+    		    ?y vivo:contains ?competency
+    	    }}
+        """
+        # executeQuey(qres2)
 
     #-----------------------------------competency q3-------------------------------------------------
     # Which courses at [UNIVERSITY] teaches [TOPIC]
-    university = None
-    topic = None
-    qres3 = f"""
-    	SELECT ?course
-    	WHERE {{
-    		?x rdfs:label '{university}'
-    		?x vivo:offers ?course
-    		?course vivo:Title '{topic}'
-    	}}
-    """
-    #executeQuey(qres3)
+    elif re.search("^Which courses at.", user_input):
+        trimmed = re.sub("[.,?!]", "", user_input)
+        university = re.split("\s", trimmed)[3]
+        topic = re.split("\s", trimmed)[len(re.split("\s", trimmed)) - 1]
+        qres3 = f"""
+        	SELECT ?course
+        	WHERE {{
+        		?x rdfs:label '{university}'
+        		?x vivo:offers ?course
+        		?course vivo:Title '{topic}'
+        	}}
+        """
+        #executeQuey(qres3)
 
+    #-----------------------------------competency q4-------------------------------------------------
+    # What are all the courses for [COURSE NAME]
+    elif re.search("^What are all the courses for.", user_input):
+        trimmed = re.sub("[.,?!]", "", user_input)
+        courseName = re.split("\s",trimmed)[len(re.split("\s",trimmed)) - 1]
+        qres4 = f"""
+	        SELECT DISTINCT ?course ?courseNumber
+	        WHERE {{
+		        ?course vivo:hasSubjectArea '{courseName}'
+		        ?course vivo:catalog ?courseNumber
+	        }}
+        """
+        #executeQuey(qres4)
+
+    #-----------------------------------competency q5-------------------------------------------------
+    # How many students are registered for [COURSE NAME][COURSE NUMBER]
+    elif re.search("^How many students are registered for.", user_input):
+        trimmed = re.sub("[.,?!]", "", user_input)
+        courseName = re.split("\s",trimmed)[len(re.split("\s",trimmed)) - 2]
+        courseNumber = re.split("\s",trimmed)[len(re.split("\s",trimmed)) - 1]
+        qres5 = f"""
+	        SELECT (COUNT(?student) as ?numberOfStudents) 
+	        WHERE {{
+	    	    ?student docu:HasTaken ?x
+    		    ?x vivo:hasSubjectArea '{courseName}'
+	    	    ?x vivo:Catalog '{courseNumber}'
+	     }}
+        """
+        #executeQuey(qres5)
+
+    #-----------------------------------competency q6-------------------------------------------------
+    # Is [COURSE NAME][COURSE NUMBER] offered by [UNIVERSITY]
+    elif re.search("^Is.*offered by.", user_input): # Need to better differentiate from Q7 below
+        trimmed = re.sub("[.,?!]", "", user_input)
+        university = re.split("\s",trimmed)[len(re.split("\s",trimmed)) - 1]
+        courseName = re.split("\s",trimmed)[1]
+        courseNumber = re.split("\s",trimmed)[2]
+        qres6 = f"""
+            ASK {{
+                ?y rdfs:label '{university}'
+	            ?y vivo:offers ?x
+	            ?x vivo:hasSubjectArea '{courseName}'
+	            ?x vivo:catalog '{courseNumber}'
+	        }}
+        """
+        # executeQuey(qres6)
+
+    #-----------------------------------competency q7-------------------------------------------------
+    # Is [STUDENT FIRSTNAME][STUDENT LASTNAME] enrolled at [UNIVERSITY]
+    elif re.search("^Is.*enrolled at.", user_input):
+        trimmed = re.sub("[.,?!]", "", user_input)
+        givenName = re.split("\s", trimmed)[1]
+        familyName = re.split("\s", trimmed)[2]
+        university = re.split("\s", trimmed)[len(re.split("\s", trimmed)) - 1]
+        qres7 = f"""
+            ASK {{
+    		    ?x foaf:givenName '{givenName}'
+    		    ?x foaf:familyName '{familyName}'
+    	        ?x vivo:Student ?y
+    	        ?y rdfs:label '{university}'
+        	}}
+        """
+        # executeQuey(qres7)
+
+    # -----------------------------------competency q8-------------------------------------------------
+    # Which universities offer [COURSE NAME][COURSE NUMBER]
+    elif re.search("^Which universities offer.", user_input):
+        trimmed = re.sub("[.,?!]", "", user_input)
+        courseName = re.split("\s", trimmed)[len(re.split("\s", trimmed)) - 2]
+        courseNumber = re.split("\s", trimmed)[len(re.split("\s", trimmed)) - 1]
+        qres8 = f"""
+    	    SELECT ?university
+    	    WHERE{{
+    	    	?university vivo:offers ?x
+        		?x vivo:hasSubjectArea '{courseName}'
+        		?x vivo:Catalog '{courseNumber}'
+        	}}
+        """
+        # executeQuey(qres8)
+
+    # -----------------------------------competency q9-------------------------------------------------
+    # What courses has [STUDENT FIRSTNAME][STUDENT LASTNAME] completed
+    elif re.search("^What courses has.*completed$", user_input):
+        trimmed = re.sub("[.,?!]", "", user_input)
+        givenName = re.split("\s", trimmed)[3]
+        familyName = re.split("\s", trimmed)[4]
+        qres9 = f"""
+    	    SELECT DISTINCT ?course
+    	    WHERE{{
+    		    ?x foaf:givenName '{givenName}'
+    		    ?x foaf:familyName '{familyName}'  
+    		    ?x docu:HasTaken ?taken
+    		    ?taken docu:grade ?grade
+    		    FILTER regex(?grade, "^") #Figure out how to REGEX to ignore anything that contains "F"
+    		    ?taken docu:refersTo ?course
+    	    }}
+        """
+        # executeQuey(qres9)
+
+    # -----------------------------------competency q10-------------------------------------------------
+    # What courses has [STUDENT FIRSTNAME][STUDENT LASTNAME] failed
+    elif re.search("^What courses has.*failed$", user_input):
+        trimmed = re.sub("[.,?!]", "", user_input)
+        givenName = re.split("\s", trimmed)[3]
+        familyName = re.split("\s", trimmed)[4]
+        qres10 = f"""
+    	    SELECT DISTINCT ?course
+    	    WHERE{{
+    	    	?x foaf:givenName '{givenName}'
+    	    	?x foaf:familyName '{familyName}'  
+    	    	?x docu:HasTaken ?taken
+    	    	?taken docu:grade 'F'
+    	    	?taken docu:refersTo ?course
+    	    }}
+        """
+        # executeQuey(qres10)
+    else:
+        print("Query not recognized")
 
 def executeQuey(query):
     #send post request to fuseki server to access dataset : Project1
