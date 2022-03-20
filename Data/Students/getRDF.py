71f4d21657ad1a9a22af97a5f490a92a485a0a39
rmanaem <rmanaem@protonmail.ch>
2022-03-19 23:35:43 -0400

    Added the files for generating student triples

---
+++ Data/Students/getRDF.py
@@ -0,0 +1,10 @@
+import os
+
+import numpy as np
+import pandas as pd
+from rdflib import Graph, Literal, RDF, Namespace, URIRef
+from rdflib.namespace import FOAF, RDFS, XSD
+
+
+if __name__ == '__main__':
+    pass
