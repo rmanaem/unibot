b712ce6f7e25dfa02c446f99d9f434c808c0aabd
Wasim Boughattas <wassooma2013@yahoo.com>
2022-03-20 16:53:59 -0400

    a lot of stuff

--- Data/focudata_getRDF.py
+++ Data/focudata_getRDF.py
@@ -31,15 +31,20 @@ if __name__ == '__main__':
     g.bind('vivo', VIVO)
     g.bind('vcard', VCARD)
 
-    # seeAlso for comp474-Lecture01
+    # add seeAlso to Lecture01 of comp474
     g.add(((URIRef(FOCUDATA + '005484_Lecture01')), RDFS.seeAlso,
            URIRef('https://www.coursehero.com/file/58197534/slides01pdf/')))
 
     # add outline and seeAlso to comp474
     comp474_uri = URIRef(FOCUDATA + 'courseID_005484')
-    g.add((comp474_uri, VIVO.description,
+    g.add((comp474_uri, FOCU.outline,
            URIRef(os.path.join(lecturesPath, 'COMP474', 'CourseInfo', 'Outline.pdf').replace('\\', '/'))))
     g.add((comp474_uri, RDFS.seeAlso,
            URIRef('https://aits.encs.concordia.ca/aits/public/top/courses/20194/05/COMP474.html')))
+
+    # add outline to comp6721
+    comp474_uri = URIRef(FOCUDATA + 'courseID_040353')
+    g.add((comp474_uri, FOCU.outline,
+           URIRef(os.path.join(lecturesPath, 'COMP6721', 'CourseInfo', 'Outline.pdf').replace('\\', '/'))))
 
     g.serialize(os.path.join(ROOT_DIR, 'Data', 'focudata.ttl'), format='turtle')
