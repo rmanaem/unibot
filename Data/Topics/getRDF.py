4619dfd8e5a63555a2d6149e2485fdc8f3b3bff2
rmanaem <rmanaem@protonmail.ch>
2022-03-20 17:04:34 -0400

    Added topics' getRDF script

---
+++ Data/Topics/getRDF.py
@@ -0,0 +1,10 @@
+import os
+
+import numpy as np
+import pandas as pd
+from rdflib import Graph, Literal, RDF, Namespace, URIRef
+from rdflib.namespace import FOAF, RDFS, XSD
+from __init__ import ROOT_DIR
+
+if __name__ == '__main__':
+    pass
