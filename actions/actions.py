a0d838000ae4970c258819f46e405a8034dccf29
n.richard <richard.n@latencetech.com>
2022-04-06 14:20:10 -0400

    Added more intent training examples

--- actions/actions.py
+++ actions/actions.py
@@ -71,11 +71,11 @@ class ActionDescribeCourse(Action):
             print(i, '----------')
             s = ''
             for index in (indexes):
-                s += index + ' ' + results[i][index]['value'] + '\n'
+                s += results[i][index]['value'] + '\n'
             print(s)
         output = s
 
-        dispatcher.utter_message(text=f"No problem, the description for {tracker.slots['course_name']} {tracker.slots['course_number']} says:\n {output}")
+        dispatcher.utter_message(text=f"No problem, the description for {tracker.slots['course_name']} {tracker.slots['course_number']} says:\n{output}")
 
         return []
 
@@ -146,12 +146,12 @@ class ActionStudentCompetency(Action):
             print(i, '----------')
             s = ''
             for index in (indexes):
-                s += index + ' ' + results[i][index]['value'] + '\n'
+                s += results[i][index]['value'] + '\n'
             print(s)
         output = s
 
         dispatcher.utter_message(
-            text=f"Sure thing, {tracker.slots['first_name']} {tracker.slots['last_name']} is competent in the following topics: \n {output}")
+            text=f"Sure thing, {tracker.slots['first_name']} {tracker.slots['last_name']} is competent in the following topics:\n{output}")
 
         return []
 
@@ -225,7 +225,7 @@ class ActionUniversityTopics(Action):
         output = s
 
         dispatcher.utter_message(
-            text=f"Okay, the following courses at {tracker.slots['university']} that teach {tracker.slots['topic']} are: \n {output}")
+            text=f"Okay, the following courses at {tracker.slots['university']} that teach {tracker.slots['topic']} are:\n{output}")
 
         return []
 
@@ -744,6 +744,82 @@ class ActionCourseReadings(Action):
 
         dispatcher.utter_message(
             text=f"Here's all the readings for {tracker.slots['course_name']} {tracker.slots['course_number']} at {tracker.slots['university']} for lecture #{tracker.slots['material_number']}:\n {output}")
+
+        return []
+
+
+class ActionTopicsCovered(Action):
+
+    def name(self) -> Text:
+        return "action_topics_covered"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        prefixes = """
+            PREFIX dbr: <http://dbpedia.org/resource/> 
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+            PREFIX focu: <http://focu.io/schema#>
+            PREFIX focudata: <http://focu.io/data#> 
+            PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+            PREFIX vcard: <http://www.w3.org/2006/vcard/ns#> 
+            PREFIX wikidata: <http://www.wikidata.org/entity/>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            PREFIX vivo: <http://vivoweb.org/ontology/core#>
+            PREFIX bibo: <http://purl.org/ontology/bibo/>
+        """
+        # EDIT QUERY
+        query = f"""
+                SELECT ?label
+                WHERE {{
+                
+                    ?course vivo:hasSubjectArea ?subject.
+                    ?course vivo:Catalog ?courseNum .
+                
+                    ?course focu:hasContent ?lecture.
+                    ?lecture bibo:number ?lecNum.
+                    ?lecture vivo:contains ?x.
+                    ?x focu:covers ?topic .
+                    ?topic rdfs:label ?label
+                    
+                    FILTER(?lecNum = {tracker.slots['material_number']})
+                    FILTER(?subject = '{tracker.slots['course_name']}')
+                    FILTER(?courseNum = {tracker.slots['course_number']})
+                }}
+                """
+        beg = query.find('SELECT') + 6  # get the index right after the word 'SELECT'
+        end = query.find('WHERE')  # get the index at 'WHERE' from the query
+        indexes = query[beg:end]  # get the string between SELECT and WHERE
+        indexes = indexes.split()  # split each element that we are trying ot obtain from query
+        i = 0
+        while i in range(len(indexes)):
+            if indexes[i][0] != '?':  # if the first index is not '?', we delete this index
+                del indexes[i]
+            else:
+                indexes[i] = indexes[i][1:]  # if it is an actual element, we substract the '?'
+                if indexes[i][-1] == ')':  # if this element ends with ')', extract the ')'
+                    indexes[i] = indexes[i][:-1]
+                i += 1
+        print(indexes)
+        query = prefixes + query
+        response = requests.post('http://localhost:3030/Project1/sparql',
+                                 data={'query': query})
+
+        res = response.json()
+        # Prints the response of the SPARQL query
+        results = res['results']['bindings']
+        for i in range(len(results)):
+            print(i, '----------')
+            s = ''
+            for index in (indexes):
+                s += index + ' ' + results[i][index]['value'] + '\n'
+            print(s)
+        output = s
+
+        dispatcher.utter_message(
+            text=f"These are the topics covered in {tracker.slots['course_event']} {tracker.slots['material_number']} of {tracker.slots['course_name']} {tracker.slots['course_number']}:\n {output}")
 
         return []
 
