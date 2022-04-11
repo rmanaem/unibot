1b6eb3a4581986244bd48fac1481c4af782ae5a0
rmanaem <rmanaem@protonmail.ch>
2022-04-11 18:07:20 -0400

    Updated the script The script will now utilize extract_ne_list and spotlight_over_text functions to generate topics triples.

--- Data/Topics/topics_getRDF.py
+++ Data/Topics/topics_getRDF.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from rdflib import Graph, Literal, RDF, Namespace, URIRef
 from rdflib.namespace import FOAF, RDFS, XSD
 
-from Utils.utils import extract_ne, DBLookup
+from Utils.utils import extract_ne_list, spotlight_over_text
 from __init__ import ROOT_DIR
 import os
 
@@ -36,49 +36,43 @@ if __name__ == '__main__':
     # automated extraction of topics from slides and worksheets
     for idx, path in enumerate([comp474_lecturesPath, comp6721_lecturePath]):
         for lectureNum in range(1, 8):
-            slideURI = URIRef(os.path.join(path, 'Slides', 'slides' + "%02d" % lectureNum + '.pdf').replace('\\', '/'))
+            slideURI = URIRef(os.path.join(
+                path, 'Slides', 'slides' + "%02d" % lectureNum + '.pdf').replace('\\', '/'))
             worksheetURI = URIRef(os.path.join(path, 'Worksheets',
                                                'worksheet' + "%02d" % lectureNum + '.pdf').replace('\\', '/'))
-            labURI = URIRef(os.path.join(path, 'Labs', 'lab' + "%02d" % lectureNum + '.pdf').replace('\\', '/'))
-            outlineURI = URIRef(os.path.join(path, 'CourseInfo', 'Outline' + '.pdf').replace('\\', '/'))
+            labURI = URIRef(os.path.join(path, 'Labs', 'lab' + "%02d" %
+                            lectureNum + '.pdf').replace('\\', '/'))
+            outlineURI = URIRef(os.path.join(
+                path, 'CourseInfo', 'Outline' + '.pdf').replace('\\', '/'))
 
             for source in [('slides', 'Slides'), ('worksheet', 'Worksheets'), ('lab', 'Labs'),
                            ('Outline', 'CourseInfo')]:
                 if source[0] == 'Outline':
                     if lectureNum == 1:
-                        filePath = os.path.join(path, source[1], source[0] + '.txt')
+                        filePath = os.path.join(
+                            path, source[1], source[0] + '.pdf')
                     else:
                         continue
                 else:
-                    filePath = os.path.join(path, source[1], source[0] + "%02d" % lectureNum + '.txt')
+                    filePath = os.path.join(
+                        path, source[1], source[0] + "%02d" % lectureNum + '.pdf')
 
                 # extract topics from pdf
-                topics = extract_ne(filePath)
+                topics = extract_ne_list(spotlight_over_text(filePath))
 
                 for i, topic in enumerate(topics):
                     if idx == 0:
-                        uniqueID = str(hash(('COMP474' + topic)))[1:7]
+                        uniqueID = str(hash(('COMP474' + topic[1])))[1:7]
                     else:
-                        uniqueID = str(hash(('COMP6721' + topic)))[1:7]
+                        uniqueID = str(hash(('COMP6721' + topic[1])))[1:7]
 
-                    # lookup dbpedia URI and label
-                    dbpediaURI, dbpediaLabel = None, 'None'
-                    try:
-                        dbpediaURI, dbpediaLabel = DBLookup(topic)
-                    except Exception:
-                        print('bug')
-
-                    try:
-                        print(Path(filePath).stem, f'\ntopic{i} :', topic, '\ndbpediaURI', dbpediaURI,
-                              '\ndbpedialabel :', dbpediaLabel, '\n')
-                    except UnicodeEncodeError:
-                        print('cant print in this python console, but it doesnt affect the rest of code')
-
-                    if dbpediaURI is None:
-                        continue
+                    dbpediaURI, dbpediaLabel = topic[0], topic[1]
 
                     # convert URI to URIRef object
                     dbpediaURI = URIRef(dbpediaURI)
+
+                    print(Path(filePath).stem, f'\ntopic{i} :', topic, '\ndbpediaURI',
+                          dbpediaURI, '\ndbpedialabel :', dbpediaLabel, '\n')
 
                     sourceURI = slideURI  # default value
                     if source[0] == 'slides':
@@ -93,7 +87,8 @@ if __name__ == '__main__':
                     # update graph
                     g.add((sourceURI, FOCU.covers, dbpediaURI))
                     g.add((dbpediaURI, RDF.type, FOCU.topic))
-                    g.add((dbpediaURI, RDFS.label, Literal(dbpediaLabel, datatype=XSD.string)))
+                    g.add((dbpediaURI, RDFS.label, Literal(
+                        dbpediaLabel, datatype=XSD.string)))
 
     lectures_pathname = os.path.join(topicsPath, 'topics.ttl')
     g.serialize(lectures_pathname, format='turtle')
